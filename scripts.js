e84adf45211eefe4ed81a9dc5578cde39edbd98e
Campaign <thanspan0@gmail.com>
2026-03-08 21:26:11 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function videoFunction(){
+            document.getElementById("myComment").innerHTML += "A great vacation video";
+}
+
+function emailMessage(){
+    document.getElementById("emailBox").innerHTML += " Enter a valid email address or eat rocks";
+}
+
+function blankMessage(){
+    document.getElementById("emailBox").innerHTML ="Email:";
+}
